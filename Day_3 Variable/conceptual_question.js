6766f8e8c88dc5aa04a222fae603f63e0e9f48be
omkar <omkarsp20@gmail.com>
2026-04-20 20:16:16 +0530

    Explore Some Conceptual Understanding

---
+++ Day_3 Variable/conceptual_question.js
@@ -0,0 +1,80 @@
+// What is a variable?
+/*  
+    Variable is a container which used to store or hold a value.
+*/
+
+
+// Why do we use variables?
+/*
+    We used a variable to store a value so we can reused it later in our program.
+*/
+
+
+// Difference between declaration and assignment?
+/*
+    declaration :
+        1.declare means we are just declare a variable by using let,var or const keyword but not yet initialize it.
+        2.Example : let city;
+
+    assignment :
+        1.assignment means we are assigning the right hand side value to the left hand side variable.
+        2.Example : let name = "kiran"
+*/
+
+
+// What are the 3 ways to declare variables?
+/*
+    There are 3 ways to declare a variable in javascript 
+        1.let
+        2.var
+        3.const
+*/
+
+
+// Which keyword allows reassignment?
+/*
+    let and var this only 2 keywords allowd reassignment.
+*/
+
+
+// Which keyword does NOT allow reassignment?
+/*
+    const does not allow reassignment.
+*/
+
+
+// What is redeclaration?
+/*
+    Suppose we declare a variable and initialize it and on next line we declare a variable with same name and assign a value that is known as redeclaration. 
+*/
+
+
+// Which keyword allows redeclaration?
+/*
+    Only var keyword is allowed redeclaration.
+    let and const does not support redeclaration.
+*/
+
+
+// What is scope (basic idea)?
+/*
+    Scope is the acutal region or area in which we can access the variable.
+    There are 2 types of scope global[function] or local[block]
+
+    let and const having a local scope or block scope
+    var having a global scope or function scope.
+*/
+
+
+// Difference between let and const?
+/*
+1.let :- 
+        a.let is a new way to declare a variable in javascript.
+        b.let is a block scope variable so the variable does not accessable outside the block.
+        c.variable declare by using let keyword can be reassign the values but cannot be redeclared.
+
+2.const :- 
+        a.const is also a new way to declare a variable in javascript.
+        b.const is a block scope variable so the variable does not accessable outside the block.
+        c.variable declared by using the const keyword cannot be reassign or redeclared.
+*/
