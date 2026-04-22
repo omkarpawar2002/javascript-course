cd4af78a1f28833382cda163db2f8720dbbc7d14
omkar <omkarsp20@gmail.com>
2026-04-22 18:08:58 +0530

    Solve Some Tasks

---
+++ Day_4 Primitive Data Types/logic_problems.js
@@ -0,0 +1,57 @@
+// 1. Create a string variable
+let firstName = "John";
+console.log(firstName);
+
+
+// 2. Create a number variable
+let employeeId = 1002;
+console.log(employeeId);
+
+
+// 3. Create a boolean variable
+let isAvailable = false;
+console.log(isAvailable);
+
+
+// 4. Create an undefined variable
+let result;
+console.log(result);
+
+
+// 5. Create a null variable
+let email = null;
+console.log(email);
+
+
+// 6. Create a bigint variable
+let population = 834673487823782n;
+console.log(population);
+
+
+// 7. Create a symbol variable
+let id = Symbol("Id");
+console.log(id);
+
+
+// 8. Check type of each variable
+console.log(typeof(firstName));
+console.log(typeof(employeeId));
+console.log(typeof(isAvailable));
+console.log(typeof(result));
+console.log(typeof(email));
+console.log(typeof(population));
+console.log(typeof(id));
+
+
+// 9. Store a value and change its type
+let marks = 89.34;
+console.log(marks);
+marks = 96.34;
+console.log(marks);
+
+
+// 10. Print type after each change
+let price = 999.99;
+console.log(price,typeof(price));
+price = "Nine Hundred Ninty-Nine";
+console.log(price,typeof(price));
