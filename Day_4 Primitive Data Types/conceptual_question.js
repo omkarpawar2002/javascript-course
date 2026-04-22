c488413c095da8605097e3337d675c32acbd5dd4
omkar <omkarsp20@gmail.com>
2026-04-22 18:08:22 +0530

    Dry And Run Conceptual Question

---
+++ Day_4 Primitive Data Types/conceptual_question.js
@@ -0,0 +1,80 @@
+// 1. What is a data type?
+/*
+    Data Type Actually refered to which kind of value a variable holds.
+*/
+
+
+// 2. What is dynamic typing?
+/*
+    Here in javascript we no need to define the data type of variable.
+    Variable value change anytime that's known as dynamic typing.
+*/
+
+
+// 3. What does typeof do?
+/*
+    typeof() operator is used to check the data type of variable.
+*/
+
+
+// 4. List all primitive data types
+/*
+    1.Primitive data types are those who are immutable and hold single value.
+    2.There are 7 types of primitive data types
+        a.string
+        b.number
+        c.boolean
+        d.undefined
+        e.null
+        f.BigInt
+        g.Symbol
+*/
+
+
+// 5. What is the type of string?
+/*
+    type of string is string.
+*/
+
+
+// 6. What is the type of number?
+/*
+    type of number is number.
+*/
+
+
+// 7. What is the type of boolean?
+/*
+    type of boolean is boolean.
+*/
+
+
+// 8. What is the type of undefined?
+/*
+    type of undefined is undefined.
+*/
+
+
+// 9. What is the type of null?
+/*
+    type of null is object and it's a javascript sweet bug.
+*/
+
+
+// 10. What is bigint?
+/*
+    1.BigInt data type used when we want to work with large number.
+    2.Number always end with n.
+*/
+
+
+// 11. What is symbol (basic idea)?
+/*
+    symbol data type is used to create unique identifiers.
+*/
+
+
+// 12. Can a variable change its type?
+/*
+    Yes, javascript is a dynamically type programming language so variable can change anytime
+*/
