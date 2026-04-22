2f0ea4ca2e744ae9b7b3bf7501fd5d83b1aa6449
omkar <omkarsp20@gmail.com>
2026-04-22 17:14:17 +0530

    Explore Coding Tasks

---
+++ Day_3 Variable/coding_problems.js
@@ -0,0 +1,130 @@
+// Declare a variable
+let first_name;
+
+
+// Assign a value later
+let last_name;
+last_name = "candel";
+
+
+// Print a variable
+let lastName = "root"
+console.log(lastName);
+
+
+// Create 3 variables
+let num1 = 10;
+let num2 = 11;
+let num3 = 12;
+console.log(num1,num2,num3);
+
+
+// Store your city
+let city = "Mumbai";
+console.log(city);
+
+
+// Store your favorite color
+let favourite_color = "olive green";
+console.log(favourite_color);
+
+
+// Store a number
+let rollNo = 121;
+console.log(rollNo);
+
+
+// Change the number
+let number = 102
+number = 201;
+console.log(number);
+
+
+// Store two variables and print both
+let name = "kiran";
+let age = 21;
+console.log("My Name is " + name + " and age is " + age);
+
+
+// Create variable without value
+let checkValue;
+console.log(checkValue);
+
+
+// Declare using let and change value
+let marks = 89.34;
+marks = 78.45;
+console.log(marks);
+
+
+// Declare using const and try changing
+const price = 89.34;
+// price = 78.45;  // Here we get an error 
+console.log(price);
+
+
+// Declare using var twice
+var name1 = "andrew";
+var name1 = "root";
+console.log(name1);
+
+
+// Create 5 variables and update all
+let result1 = 89.37;
+let result2 = 65.34;
+let result3 = 54.23;
+let result4 = 34.65;
+let result5 = 40.54;
+result1 = "Pass";
+result2 = "Fail";
+result3 = "Fail";
+result4 = "Pass";
+result5 = "Pass";
+console.log(result1,result2,result3,result4,result5);
+
+
+// Copy one variable into another
+let asianCountry = "India";
+let bestCountry = asianCountry;
+console.log(bestCountry);
+
+
+// Create variable inside block {}
+{
+    let capital = "delhi";
+}
+
+
+// Try accessing it outside
+{
+    let capital = "delhi";
+}
+// console.log(capital);  // Here we get an reference Error
+
+
+// Create 10 variables with meaningful names
+/*
+let firstName = "Aaradhya";
+let lastName = "Shriwastav";
+let age = 25;
+let city = "Pune";
+let state = "Maharashtra";
+let country = "India";
+let phoneNo = "783468";
+let isStudent = true;
+let result = "Pass";
+let email = "aaradhya32@gmail.com";
+*/
+
+
+// Create 3 variables and swap values manually
+let updateFirst = 101;
+let updateSecond = 201;
+let updateThird = 301;
+temp = updateFirst;
+updateFirst = updateSecond;
+updateSecond = updateThird;
+updateThird = temp;
+console.log(updateFirst);
+console.log(updateSecond);
+console.log(updateThird);
