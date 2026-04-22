63db74c341d5ad9fb889ea0f8cea6d5e068b5c48
omkar <omkarsp20@gmail.com>
2026-04-22 17:13:54 +0530

    Solve Some Logical Problems

---
+++ Day_3 Variable/logic_problems.js
@@ -0,0 +1,64 @@
+// Create a variable and store your name
+let firstName = "Joe";
+console.log(firstName);
+
+
+// Create a variable and store your age
+let age = 25;
+console.log(age);
+
+
+// Change your age value
+let myAge = 25;
+myAge = 28;
+console.log(myAge);
+
+
+// Create two variables and store numbers
+let num1 = 34;
+let num2 = 43;
+console.log(num1);
+console.log(num2);
+
+
+// Change both values
+let num3 = 101;
+let num4 = 201;
+num3 = 1000;
+num4 = 2000;
+console.log(num3);
+console.log(num4);
+
+
+// Copy value from one variable to another
+let num5 = 45;
+let num6 = num5;
+console.log(num6);
+
+
+// Store a sentence using variables
+let city = "Mumbai";
+let country = "India";
+console.log(city,"is in",country);
+
+
+// Create a variable, change it 3 times
+let marks = 87.78;
+marks = 78.45;
+marks = 45.56;
+marks = 89.35;
+console.log(marks);
+
+
+// Store a number and increase it manually
+let num7 = 78;
+console.log(num7);
+num7 = num7 + 1;
+console.log(num7);
+
+
+// Store a number and decrease it manually
+let num8 = 78;
+console.log(num8);
+num8 = num8 - 1;
+console.log(num8);
